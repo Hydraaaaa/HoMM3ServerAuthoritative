3be5bdccb1db51601cf2b04a2a43204034d46a3c
James da Silva <james.dasilva1111@gmail.com>
2020-09-17 20:20:52 +1000

    Added list of selectable maps, need to add scrolling

---
+++ Assets/Scripts/ScenarioEntry.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenarioEntry : MonoBehaviour
+{
+    public ScenarioList ScenarioList { get; set; }
+    public Text ScenarioName => m_ScenarioName;
+    public Map Map { get; private set; }
+    [SerializeField] Text m_ScenarioName;
+    [SerializeField] Color m_SelectedColor;
+
+    public void SetMap(Map a_Map)
+    {
+        Map = a_Map;
+
+        Debug.Log(a_Map);
+
+        if (a_Map != null)
+        {
+            m_ScenarioName.text = a_Map.Name;
+        }
+        else
+        {
+            m_ScenarioName.text = "";
+        }
+    }
+    
+    public void SetSelected(Map a_Map)
+    {
+        if (a_Map == Map)
+        {
+            m_ScenarioName.color = m_SelectedColor;
+        }
+        else
+        {
+            m_ScenarioName.color = Color.white;
+        }
+    }
+
+    public void OnClick()
+    {
+        ScenarioList.EntryClicked(Map);
+    }
+}
---
+++ Assets/Scripts/ScenarioList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenarioList : MonoBehaviour
+{
+    [SerializeField] Map[] m_Maps;
+    [SerializeField] ScenarioEntry[] m_ScenarioEntries;
+    [SerializeField] Text m_Description;
+
+    Map m_SelectedMap;
+
+    void Awake()
+    {
+        for (int i = 0; i < m_ScenarioEntries.Length; i++)
+        {
+            m_ScenarioEntries[i].ScenarioList = this;
+        }
+
+        PopulateScenarioEntries(0);
+    }
+    
+    void PopulateScenarioEntries(int a_Offset)
+    {
+        int _EntriesToPopulate = Mathf.Min(m_ScenarioEntries.Length, m_Maps.Length - a_Offset);
+
+        for (int i = 0; i < _EntriesToPopulate; i++)
+        {
+            m_ScenarioEntries[i].SetMap(m_Maps[i + a_Offset]);
+            m_ScenarioEntries[i].SetSelected(m_SelectedMap);
+        }
+
+        for (int i = _EntriesToPopulate; i < m_ScenarioEntries.Length; i++)
+        {
+            m_ScenarioEntries[i].SetMap(null);
+        }
+    }
+
+    public void EntryClicked(Map a_Map)
+    {
+        m_SelectedMap = a_Map;
+
+        m_Description.text = a_Map.Description;
+
+        for (int i = 0; i < m_ScenarioEntries.Length; i++)
+        {
+            m_ScenarioEntries[i].SetSelected(m_SelectedMap);
+        }
+    }
+}
