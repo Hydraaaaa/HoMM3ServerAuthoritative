180d7c0d27a47eb5472586e24741fb781311da60
James da Silva <james@blowfishstudios.com>
2020-10-13 12:56:07 +1100

    Implemented heroes 3 style camera panning

--- Assets/Scripts/Game/CameraControl.cs
+++ Assets/Scripts/Game/CameraControl.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-
     [SerializeField] GameSettings m_GameSettings = null;
 
     [Space]
@@ -13,10 +12,22 @@ public class CameraControl : MonoBehaviour
 
     bool m_Active;
 
+    float m_MoveCooldown = 0.07f;
+    float m_CurrentMoveCooldown;
+    bool m_IsMoving;
+
     void Awake()
     {
         Cursor.lockState = CursorLockMode.Confined;
         m_Active = true;
+        m_IsMoving = false;
+
+        transform.position = new Vector3
+        (
+            Mathf.Round(transform.position.x),
+            Mathf.Round(transform.position.y),
+            transform.position.z
+        );
     }
 
     void Update()
@@ -35,30 +46,52 @@ public class CameraControl : MonoBehaviour
 
         if (m_Active)
         {
+            bool _IsMoving = false;
+
             Vector3 _Position = transform.position;
 
             if (Input.mousePosition.x <= 2)
             {
-                _Position.x -= m_PanSpeed * Time.deltaTime;
+                _Position.x -= 2;
+                _IsMoving = true;
             }
             else if (Input.mousePosition.x >= Screen.width - 3)
             {
-                _Position.x += m_PanSpeed * Time.deltaTime;
+                _Position.x += 2;
+                _IsMoving = true;
             }
 
             if (Input.mousePosition.y <= 2)
             {
-                _Position.y -= m_PanSpeed * Time.deltaTime;
+                _Position.y -= 2;
+                _IsMoving = true;
             }
             else if (Input.mousePosition.y >= Screen.height - 3)
             {
-                _Position.y += m_PanSpeed * Time.deltaTime;
+                _Position.y += 2;
+                _IsMoving = true;
             }
 
-            _Position.x = Mathf.Clamp(_Position.x, -0.5f, m_GameSettings.Scenario.Size - 0.5f);
-            _Position.y = Mathf.Clamp(_Position.y, -m_GameSettings.Scenario.Size - 0.5f, 0.5f);
+            m_IsMoving = _IsMoving;
 
-            transform.position = _Position;
+            if (!m_IsMoving)
+            {
+                m_CurrentMoveCooldown = 0;
+            }
+            else
+            {
+                m_CurrentMoveCooldown -= Time.deltaTime;
+
+                if (m_CurrentMoveCooldown <= 0)
+                {
+                    m_CurrentMoveCooldown += m_MoveCooldown;
+
+                    _Position.x = Mathf.Clamp(_Position.x, -0.5f, m_GameSettings.Scenario.Size - 0.5f);
+                    _Position.y = Mathf.Clamp(_Position.y, -m_GameSettings.Scenario.Size - 0.5f, 0.5f);
+
+                    transform.position = _Position;
+                }
+            }
         }
     }
 }
