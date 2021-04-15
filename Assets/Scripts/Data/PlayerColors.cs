76f7a13311e4ad74ad75e59db7a0d2b89499e8a0
James da Silva <james.dasilva1111@gmail.com>
2021-04-16 09:55:06 +1000

    Added GUI sidebar button sprites

--- Assets/Scripts/Data/PlayerColors.cs
+++ Assets/Scripts/Data/PlayerColors.cs
@@ -20,6 +20,56 @@ public class PlayerColors : ScriptableObject
         public Sprite Resources;
         public Sprite BottomBarFill;
         public Sprite Date;
+
+        // Buttons
+        public Sprite NextHero;
+        public Sprite NextHeroDisabled;
+        public Sprite NextHeroHighlighted;
+        public Sprite NextHeroPressed;
+        public Sprite EndTurn;
+        public Sprite EndTurnDisabled;
+        public Sprite EndTurnHighlighted;
+        public Sprite EndTurnPressed;
+        public Sprite KingdomOverview;
+        public Sprite KingdomOverviewDisabled;
+        public Sprite KingdomOverviewHighlighted;
+        public Sprite KingdomOverviewPressed;
+        public Sprite Underground;
+        public Sprite UndergroundDisabled;
+        public Sprite UndergroundHighlighted;
+        public Sprite UndergroundPressed;
+        public Sprite Overworld;
+        public Sprite OverworldDisabled;
+        public Sprite OverworldHighlighted;
+        public Sprite OverworldPressed;
+        public Sprite QuestLog;
+        public Sprite QuestLogDisabled;
+        public Sprite QuestLogHighlighted;
+        public Sprite QuestLogPressed;
+        public Sprite SleepHero;
+        public Sprite SleepHeroDisabled;
+        public Sprite SleepHeroHighlighted;
+        public Sprite SleepHeroPressed;
+        public Sprite WakeHero;
+        public Sprite WakeHeroDisabled;
+        public Sprite WakeHeroHighlighted;
+        public Sprite WakeHeroPressed;
+        public Sprite MoveHero;
+        public Sprite MoveHeroDisabled;
+        public Sprite MoveHeroHighlighted;
+        public Sprite MoveHeroPressed;
+        public Sprite Spellbook;
+        public Sprite SpellbookDisabled;
+        public Sprite SpellbookHighlighted;
+        public Sprite SpellbookPressed;
+        public Sprite AdventureOptions;
+        public Sprite AdventureOptionsDisabled;
+        public Sprite AdventureOptionsHighlighted;
+        public Sprite AdventureOptionsPressed;
+        public Sprite SystemOptions;
+        public Sprite SystemOptionsDisabled;
+        public Sprite SystemOptionsHighlighted;
+        public Sprite SystemOptionsPressed;
     }
 
     public PlayerElements[] Elements;
