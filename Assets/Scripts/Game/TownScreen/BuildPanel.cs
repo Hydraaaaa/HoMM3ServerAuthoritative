f11295788f908037646df9639ba7c03b27d58c68
James da Silva <james.dasilva1111@gmail.com>
2021-10-28 14:05:49 +1100

    Added resource requirements on build panel

--- Assets/Scripts/Game/TownScreen/BuildPanel.cs
+++ Assets/Scripts/Game/TownScreen/BuildPanel.cs
@@ -13,6 +13,18 @@ public class BuildPanel : MonoBehaviour
     [SerializeField] Text m_Requirements;
     [SerializeField] Button m_BuildButton;
     [SerializeField] Button m_CancelButton;
+    [SerializeField] Image[] m_ResourceImages;
+    [SerializeField] Text[] m_ResourceAmounts;
+
+    [Space]
+
+    [SerializeField] Sprite m_GoldSprite;
+    [SerializeField] Sprite m_WoodSprite;
+    [SerializeField] Sprite m_OreSprite;
+    [SerializeField] Sprite m_MercurySprite;
+    [SerializeField] Sprite m_SulfurSprite;
+    [SerializeField] Sprite m_CrystalsSprite;
+    [SerializeField] Sprite m_GemsSprite;
 
     BuildingData m_BuildingData;
 
@@ -27,7 +39,7 @@ public class BuildPanel : MonoBehaviour
 
         m_BuildingData = a_Building.BuildingData;
 
-        m_Title.text = m_BuildingData.DisplayName;
+        m_Title.text = "Build " + m_BuildingData.DisplayName;
         m_Image.sprite = a_Building.Image.sprite;
         m_Description.text = m_BuildingData.Description;
         m_Requirements.text = "Requires:\n";
@@ -61,6 +73,101 @@ public class BuildPanel : MonoBehaviour
         }
 
         m_BuildButton.interactable = a_Building.Buildable;
+
+        int _ResourceCount = 0;
+
+        if (a_Building.BuildingData.WoodCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_WoodSprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.WoodCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (a_Building.BuildingData.MercuryCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_MercurySprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.MercuryCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (a_Building.BuildingData.OreCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_OreSprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.OreCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (a_Building.BuildingData.SulfurCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_SulfurSprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.SulfurCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (a_Building.BuildingData.CrystalCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_CrystalsSprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.CrystalCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (a_Building.BuildingData.GemCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_GemsSprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.GemCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (a_Building.BuildingData.GoldCost > 0)
+        {
+            m_ResourceImages[_ResourceCount].sprite = m_GoldSprite;
+            m_ResourceAmounts[_ResourceCount].text = a_Building.BuildingData.GoldCost.ToString();
+            _ResourceCount++;
+        }
+
+        if (_ResourceCount > 3)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                m_ResourceImages[i].rectTransform.anchoredPosition = new Vector2(80 * (i - 1.5f), -59);
+                m_ResourceAmounts[i].rectTransform.anchoredPosition = new Vector2(80 * (i - 1.5f), -83);
+
+                m_ResourceImages[i].gameObject.SetActive(true);
+                m_ResourceAmounts[i].gameObject.SetActive(true);
+            }
+
+            for (int i = 4; i < _ResourceCount; i++)
+            {
+                m_ResourceImages[i].rectTransform.anchoredPosition = new Vector2(80 * ((i-4) - (_ResourceCount-5) / 2.0f), -133);
+                m_ResourceAmounts[i].rectTransform.anchoredPosition = new Vector2(80 * ((i-4) - (_ResourceCount-5) / 2.0f), -157);
+
+                m_ResourceImages[i].gameObject.SetActive(true);
+                m_ResourceAmounts[i].gameObject.SetActive(true);
+            }
+
+            for (int i = _ResourceCount; i < 7; i++)
+            {
+                m_ResourceImages[i].gameObject.SetActive(false);
+                m_ResourceAmounts[i].gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _ResourceCount; i++)
+            {
+                m_ResourceImages[i].rectTransform.anchoredPosition = new Vector2(80 * (i - (_ResourceCount-1) / 2.0f), -96);
+                m_ResourceAmounts[i].rectTransform.anchoredPosition = new Vector2(80 * (i - (_ResourceCount-1) / 2.0f), -120);
+
+                m_ResourceImages[i].gameObject.SetActive(true);
+                m_ResourceAmounts[i].gameObject.SetActive(true);
+            }
+
+            for (int i = _ResourceCount; i < 7; i++)
+            {
+                m_ResourceImages[i].gameObject.SetActive(false);
+                m_ResourceAmounts[i].gameObject.SetActive(false);
+            }
+        }
     }
 
     public void Close()
